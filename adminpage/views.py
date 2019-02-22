0e8596879bf3cabe515f397e109f62988ac88bd4
nikkirfernandez <47142187+nikkirfernandez@users.noreply.github.com>
2019-02-22 14:36:15 +0800

    Update views.py

--- adminpage/views.py
+++ adminpage/views.py
@@ -1,3 +1,10 @@
+# This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, Diliman for the AY 2018-2019.
+
+# CODE HISTORY #
+# Kasilag     
+
+# File creation date: Feb. 19, 2019
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
