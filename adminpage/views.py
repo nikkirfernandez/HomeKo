3643920f61861fd11f56980950aac027145f93b0
Nikki Sontillano <sontillano.nicole@gmail.com>
2019-03-07 00:17:22 +0800

    updated links on html files

--- adminpage/views.py
+++ adminpage/views.py
@@ -8,6 +8,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.db.models.functions import Concat
 
 from .choices import *
 from .forms import *
@@ -35,19 +36,58 @@ def tablePage(request, table):
 	# recordPK is a list of the primary keys 
 	# recordName is a list of names that represent the records
     # records = [{'item1': t[0], 'item2': t[1]} for t in zip(recordPK, recordName)]
-	if table=="Housing":
+
+	if table == "Area":
+		recordPK = Area.objects.values_list('areaid', flat=True).order_by('areaid')
+		recordName = Area.objects.values_list('areaname', flat=True).order_by('areaid')
+	elif table=="Housing":
 		recordPK = Housing.objects.values_list('housingid', flat=True).order_by('housingid')
 		recordName = Housing.objects.values_list('housingname', flat=True).order_by('housingid')		
 	elif table=="Additionalinfo":
 		recordPK = Additionalinfo.objects.values_list('additionalinfoid', flat=True).order_by('additionalinfoid')
 		recordName = Additionalinfo.objects.values_list('additionalinfoname', flat=True).order_by('additionalinfoid')
+	elif table == "Contact":
+		recordPK = Contact.objects.values_list('contactid', flat=True).order_by('contactid')
+		recordName = Contact.objects.values_list('contactno', flat=True).order_by('contactid')
+	elif table == "Feedback":
+		######## EDIT THIS ######## RECORDNAME: HOUSINGNAME AND FIRST 5 WORDS OF COMMENT 
+		recordPK = Feedback.objects.values_list('feedbackid', flat=True).order_by('feedbackid')
+		recordName = Feedback.objects.values_list('status', flat=True).order_by('feedbackid')
+	elif table == "Housetype":
+		recordPK = Housetype.objects.values_list('housetypeid', flat=True).order_by('housetypeid')
+		recordName = Housetype.objects.values_list('housetypename', flat=True).order_by('housetypeid')
+	elif table == "HousingAdditionalinfo":
+		recordPK = HousingAdditionalInfo.objects.values_list('housingadditionalinfoid', flat=True).order_by('housingadditionalinfoid')
+		recordName = HousingAdditionalInfo.objects.values_list('housingid__housingname', 'additionalinfoid__additionalinfoname').order_by('housingadditionalinfoid')
+	elif table == "Request":
+		######## EDIT THIS ######## RECORDNAME: REQTYPE AND FIRST 5 WORDS OF REQUEST 
+		recordPK = Request.objects.values_list('requestid', flat=True).order_by('requestid')
+		recordName = Request.objects.values_list('reqtype', flat=True).order_by('requestid')
+	elif table == "HousingRequest":
+		recordPK = HousingRequest.objects.values_list('housingrequestid', flat=True).order_by('housingrequestid')
+		recordName = HousingRequest.objects.values_list('housingid__housingname', 'requestid__reqtype').order_by('housingrequestid')
+	elif table == "Owner":
+		recordPK = Owner.objects.values_list('ownerid', flat=True).order_by('ownerid')
+		recordName = Owner.objects.values_list('ownername', flat=True).order_by('ownerid')
+	elif table == "HousingOwner":
+		recordPK = HousingOwner.objects.values_list('housingownerid', flat=True).order_by('housingownerid')
+		recordName = HousingOwner.objects.values_list('ownerid__ownername', 'housingid__housingname').order_by('housingownerid')
+	elif table == "Picture": 
+		recordPK = Picture.objects.values_list('pictureid', flat=True).order_by('pictureid')
+		recordName = Picture.objects.values_list('filename', flat=True).order_by('pictureid')
+	elif table == "RoomCost":
+		recordPK = RoomCost.objects.values_list('roomid', flat=True).order_by('roomid')
+		recordName = RoomCost.objects.values_list('housingid__housingname', 'roomname').order_by('roomid')
+	elif table == "Propertytype":
+		recordPK = Propertytype.objects.values_list('propertytypeid', flat=True).order_by('propertytypeid')
+		recordName = Propertytype.objects.values_list('propertytypename', flat=True).order_by('propertytypeid')
 
 	records = [{'item1': t[0], 'item2': t[1]} for t in zip(recordPK, recordName)]
 
 	content = {
 		'tableChoices' : TABLES_CHOICES,
 		'tableName' : table,
-		'records' : records,           
+		'records' : records,          
 	}
 
 	return render(request, 'adminpage/tablePage.html', content)
