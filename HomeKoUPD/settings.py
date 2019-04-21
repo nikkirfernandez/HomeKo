5b09ef94ecd765aa21085bf2eee69dd5c36f1e3b
crkasilag <crkasilag@up.edu.ph>
2019-04-21 20:33:23 +0800

    modified database

--- HomeKoUPD/settings.py
+++ HomeKoUPD/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'HomeKoUPD.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'homeko',
-        'USER': 'crkasilag',
-        'PASSWORD': 'clarence6419',
-        'HOST': 'localhost',
+        'ENGINE': 'heroku_c56369ae8095d6a',
+        'NAME': 'homeko_heroku',
+        'USER': 'bd28bc4415515b',
+        'PASSWORD': 'b67a01ce',
+        'HOST': 'us-cdbr-iron-east-02.cleardb.net',
     }
 }
 
