fe8549021d312a98d0a24cffe91694a31641ed7e
Nikki Sontillano <sontillano.nicole@gmail.com>
2019-04-30 02:24:44 +0800

    pyc files

--- HomeKoUPD/settings.py
+++ HomeKoUPD/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'homeko',
-        'USER': 'crkasilag',
-        'PASSWORD': 'clarence6419',
+        'USER': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
     }
 }
